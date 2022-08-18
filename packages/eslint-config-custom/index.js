6d7c24d3de81e0540330d58f0f02f3e5fa997615
Mahdi_Murshed <123mahdi12345@gmail.com>
2022-08-18 10:28:40 +0600

    build: husky, eslint, storybook setup

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,17 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: [
+    "next",
+    "turbo",
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "prettier",
+  ],
+  globals: {
+    React: "readonly",
+  },
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "no-unused-vars": [1, { args: "after-used", argsIgnorePattern: "^_" }],
   },
 };
